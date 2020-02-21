ced68834c9d5734e3c1b81cc3f408cd398de7988
Maxo-Texas <stephenmcleod2004@yahoo.com>
2020-02-21 17:31:01 -0600

    First 1.12 Version

--- src/main/java/com/mactso/hbm/config/MyConfig.java
+++ src/main/java/com/mactso/hbm/config/MyConfig.java
@@ -1,116 +1,73 @@
+// 1.12.2 version
 package com.mactso.hbm.config;
 
-import org.apache.commons.lang3.tuple.Pair;
-
-import com.mactso.hbm.Main;
-
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
-import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import com.mactso.hbm.util.Reference;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.Config.Name;
+import net.minecraftforge.common.config.Config.RangeDouble;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod.EventBusSubscriber(modid = Main.MODID, bus=Mod.EventBusSubscriber.Bus.MOD)
+
+@Config(modid=Reference.MOD_ID)
+@Mod.EventBusSubscriber
 public class MyConfig
 {
-	public static final Server SERVER;
-	public static final ForgeConfigSpec SERVER_SPEC;
-	static
-	{
-		final Pair<Server, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Server::new);
-		SERVER_SPEC = specPair.getRight();
-		SERVER = specPair.getLeft();
-	}
+	@Comment( { "Exhaustion Height" } )
+	@Name ("Exhaustion Height")
+	@RangeDouble (min = 5, max=196)
+	public static double ExhaustionHeight = 48.0;
+	
+	@Comment ( { "Use Proportional Exhaustion" } )
+	@Name ("Use Proportional Exhaustion")
+	public static boolean aBooleanProportionalExhaustion = true;
+	
+	@Comment ( { "Print Debugging Messages to Log" } )
+	@Name ( "Print Debugging Messages to Log" )
+	public static boolean aBooleanDebug = false;
 
-	public static double ExhaustionHeight;
-	public static double ExhaustionAmountWoodGold;
-	public static double ExhaustionAmountStone;
-	public static double ExhaustionAmountIron;
-	public static double ExhaustionAmountDiamond;
-	public static boolean aBooleanProportionalExhaustion;
-	public static boolean aBooleanDebug;
+    @Comment( { " ",
+    	        "0 Wood Tool Exhaustion Amount" } )
+    @Name( "0 Wood Tool Exhaustion Amount"  )
+    @RangeDouble (min = .1, max=40.0)
+	public static double ExhaustionAmountWood = 8.0;
+
+    @Comment( { "1 Stone Tool Exhaustion Amount" } )
+    @Name( "1 Stone Tool Exhaustion Amount" )
+	@RangeDouble (min = .1, max=40.0)
+	public static double ExhaustionAmountStone = 4.0;
+
+    @Comment( { "2 Iron Tool Exhaustion Amount" } )
+    @Name( "2 Iron Tool Exhaustion Amount" )
+	@RangeDouble (min = .1, max=40.0)
+  	public static double ExhaustionAmountIron = 2.0;
+
+    @Comment( { " ",
+    "3 Gold Tool Exhaustion Amount" } )
+	@Name( "3 Gold Tool Exhaustion Amount"  )
+	@RangeDouble (min = .1, max=40.0)
+	public static double ExhaustionAmountGold = 1.5;    
+    
+    @Comment( { "4 Diamond Tool Exhaustion Amount" } )
+    @Name( "4 Diamond Tool Exhaustion Amount" )
+	@RangeDouble (min = .1, max=40.0)
+	public static double ExhaustionAmountDiamond = 1.0;
+
 
 	@SubscribeEvent
-	public static void onModConfigEvent(final ModConfig.ModConfigEvent configEvent)
+	public static void onModConfigEvent(OnConfigChangedEvent event)
 	{
-		if (configEvent.getConfig().getSpec() == MyConfig.SERVER_SPEC)
+		if(event.getModID().equals(Reference.MOD_ID))
 		{
-			bakeConfig();
+			ConfigManager.sync (event.getModID(), Config.Type.INSTANCE);
+			if (aBooleanDebug) {
+				System.out.println("HarderBranchMiningConfig: " + ExhaustionHeight + ", " + aBooleanProportionalExhaustion);
+
+			}
 		}
 	}
 
-	public static void bakeConfig()
-	{
-		ExhaustionHeight = SERVER.ExhaustionHeight.get();
-
-		ExhaustionAmountWoodGold = SERVER.ExhaustionAmountWood.get();
-		ExhaustionAmountStone = SERVER.ExhaustionAmountStone.get();
-		ExhaustionAmountIron = SERVER.ExhaustionAmountIron.get();
-		ExhaustionAmountDiamond = SERVER.ExhaustionAmountDiamond.get();
-	
-		aBooleanProportionalExhaustion = SERVER.aBooleanProportionalExhaustion.get();
-		aBooleanDebug = SERVER.aBooleanDebug.get();
-		
-		System.out.println("HarderBranchMiningConfig: " + ExhaustionHeight + ", " + aBooleanProportionalExhaustion);
-
-	}
-
-	public static class Server
-	{
-		public final DoubleValue ExhaustionHeight;
-		public final DoubleValue ExhaustionAmountWood;
-		public final DoubleValue ExhaustionAmountStone;
-		public final DoubleValue ExhaustionAmountIron;
-		public final DoubleValue ExhaustionAmountDiamond;
-		public final BooleanValue aBooleanProportionalExhaustion;
-		public final BooleanValue aBooleanDebug;
-
-
-		public Server(ForgeConfigSpec.Builder builder) {
-
-			builder.push("Exhaustion Height");
-			ExhaustionHeight = builder
-					.comment("Depth Extra Exhaustion Starts At")
-					.translation(Main.MODID + ".config." + "ExhaustionHeight")
-					.defineInRange("ExhaustionHeight", () -> 48.0, 5.0, 196.0);
-
-			builder.pop();
-			
-			aBooleanProportionalExhaustion = builder
-					.comment("Exhaustion Proportional to Depth or Flat")
-					.translation(Main.MODID + ".config." + "aBooleanProportionalExhaustion")
-					.define("aBooleanProportionalExhaustion", false);
-
-			aBooleanDebug = builder
-					.comment("Print Debug Message on Block Break")
-					.translation(Main.MODID + ".config." + "aBooleanDebug")
-					.define("aBooleanDebug", false);
-
-			builder.push("Tool Exhaustion");
-
-			ExhaustionAmountWood = builder
-					.comment("Wood/Diamond Tool Exhaustion Amount")
-					.translation(Main.MODID + ".config." + "ExhaustionAmountWoodGold")
-					.defineInRange("ExhaustionAmountWoodGold", () -> 8.0, 0.1, 10.0);
-
-			ExhaustionAmountStone = builder
-					.comment("Stone Tool Exhaustion Amount")
-					.translation(Main.MODID + ".config." + "ExhaustionAmountStone")
-					.defineInRange("ExhaustionAmountStone", () -> 4.0, 0.1, 10.0);
-			
-			ExhaustionAmountIron = builder
-					.comment("Iron Tool Exhaustion Amount")
-					.translation(Main.MODID + ".config." + "ExhaustionAmountIron")
-					.defineInRange("ExhaustionAmountIron", () -> 2.0, 0.1, 10.0);
-
-			ExhaustionAmountDiamond= builder
-					.comment("Diamond Tool Exhaustion Amount")
-					.translation(Main.MODID + ".config." + "ExhaustionAmountDiamond")
-					.defineInRange("ExhaustionAmountDiamond", () -> 1.0, 0.1, 10.0);
-		
-			builder.pop();
-
-		}
-	}
 }
