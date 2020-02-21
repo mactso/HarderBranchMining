265ee72f6d82194f19f62d698cfed85b87824b0f
Maxo-Texas <stephenmcleod2004@yahoo.com>
2020-02-21 17:27:30 -0600

    Add files via upload

--- src/main/java/com/mactso/hbm/Main.java
+++ src/main/java/com/mactso/hbm/Main.java
@@ -1,74 +1,29 @@
- package com.mactso.hbm;
+// 1.12.2 version
+package com.mactso.hbm;
 
-import org.lwjgl.glfw.GLFW;
-
-import com.mactso.hbm.config.MyConfig;
 import com.mactso.hbm.event.BlockBreakHandler;
-import net.minecraft.item.Item;
-import net.minecraft.item.crafting.IRecipeSerializer;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.ColorHandlerEvent;
-import net.minecraftforge.client.event.GuiScreenEvent;
+import com.mactso.hbm.util.Reference;
+
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("hbm")
-public class Main
-{
-    public static final String MODID = "hbm"; 
 
-    public Main()
-    {
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.register(new BlockBreakHandler());
-        
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER,MyConfig.SERVER_SPEC );
-    }
+	// The value here should match an entry in the META-INF/mods.toml file
+	@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+	 public class Main
+	 {
+		// , serverSideOnly=true  (research this tag more)
+		@Instance
+		public static Main instance;
 
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents
-    {
-    	@SubscribeEvent
-    	public static void onItemsRegistry(final RegistryEvent.Register<Item> event)
-    	{
-    		// event.getRegistry().register(ModItems.EA_HELMET);
+		@EventHandler
+		public void preInit (FMLPreInitializationEvent event) {
+			System.out.println("HarderBranchMining: Registering Handler");
+			MinecraftForge.EVENT_BUS.register(new BlockBreakHandler ());
+			
+		}
 
-    	}
-
-        @OnlyIn(Dist.CLIENT)
-        @SubscribeEvent
-        public static void onColorsRegistry(final ColorHandlerEvent.Item event)
-        {
-            // event.getItemColors().register((itemstack, index) -> {
-            //	return index > 0 ? -1 : ((IDyeableArmorItem)itemstack.getItem()).getColor(itemstack);
-            // }, ModItems.EA_HELMET, ModItems.EA_CHESTPLATE, ModItems.EA_LEGGINGS, ModItems.EA_BOOTS);
-        }
-
-        @SubscribeEvent
-        public static void onRecipeRegistry(final RegistryEvent.Register<IRecipeSerializer<?>> event)
-        {
-        	// event.getRegistry().register(EARecipe.CRAFTING_EA);
-        }
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    //@Mod.EventBusSubscriber()
-	public static class Events
-	{
-	    @SubscribeEvent
-	    public static void onMouseScreenEvent(GuiScreenEvent.MouseClickedEvent.Pre event)
-	    {
-	    	if (event.isCanceled())
-	    		return;
-	    	if (event.getButton() != GLFW.GLFW_MOUSE_BUTTON_RIGHT)
-	    		return;
-	    }
-	}
-}
+	 }
