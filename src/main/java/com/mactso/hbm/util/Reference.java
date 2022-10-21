168fef44469b749885a39b3f296c44ece5250977
mactso <stephenmcleod2004@yahoo.com>
2022-10-21 18:53:00 -0500

    Update MC.info file with correct version
    fixes ability to edit configuration from within the game.

--- src/main/java/com/mactso/hbm/util/Reference.java
+++ src/main/java/com/mactso/hbm/util/Reference.java
@@ -3,8 +3,8 @@ package com.mactso.hbm.util;
 
 public class Reference {
 
-	public static final String MOD_ID = "hbm2";
+	public static final String MOD_ID = "hbm";
 	public static final String NAME = "Harder Branch Mining";
-	public static final String VERSION = "2.0.0.10";
+	public static final String VERSION = "2.0.0.15";
 	public static final String ACCEPTED_VERSIONS = "1.12.2";
 }
